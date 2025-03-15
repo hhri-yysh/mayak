d4bd16f9a14ceda707e428ae9df9378ce6524c3e
hhri-yysh <heorhiiiukrainian2006@proton.me>
2025-03-15 16:15:49 +0200

    Add libs to un.h for socket and icmp support. Implement in main.c write functions: example_print(); and print_help(); for usage output and --help option.

---
+++ lib/un.h
@@ -0,0 +1,14 @@
+#ifndef __un_h
+#define __un_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <string.h>
+
+#define MAX_HOP 30
+#define TIMEOUT 5
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,5 +1,22 @@
-#include <stdio.h>
+#include "lib/un.h"
 
-int main(){
+void example_print(){
+	printf("./mayak --help\n");
+	printf("Traceroute clone created by Heorhii Yashchuk\n");	
+}
 
+void print_help(){
+	printf("Usage: ./mayak [OPTIONS]\n");
+	printf("Options:\n");
+	printf("-4\t IPv4\n");
+	printf("-6\t IPv6\n");
+}
+
+int main(int argc, char** argv){
+	if (argc <=1) {
+		example_print();
+	}
+	else if (strcmp(argv[1], "--help") == 0){
+		print_help();	
+	}
 }
