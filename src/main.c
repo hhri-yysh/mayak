aa6e9bbafdcd5b276268e7f50aaa641ce84e513e
hhri-yysh <heorhiiiukrainian2006@proton.me>
2025-03-17 00:09:58 +0200

    Update main.c: Add new functions for error output and add ip entry validation

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "../lib/un.h"
 #include "../lib/icmp.h"
+#include "../lib/socket.h"
 
 void example_print(){
 	printf("./mayak --help\n");
@@ -9,15 +10,36 @@ void example_print(){
 void print_help(){
 	printf("Usage: ./mayak [OPTIONS]\n");
 	printf("Options:\n");
-	printf("-4\t IPv4\n");
-	printf("-6\t IPv6\n");
+	printf("\t-4\t IPv4\n");
+	printf("\t-6\t IPv6\n");
+}
+
+void err_quit(const char* msg){
+	perror(msg);
+	exit(0);
+}
+
+void err_sys(const char* msg){
+	perror(msg);
+	exit(0);
 }
 
 int main(int argc, char** argv){
+	int sockfd;
+
 	if (argc <=1) {
 		example_print();
 	}
 	else if (strcmp(argv[1], "--help") == 0){
 		print_help();	
 	}
+
+	if (argc != 2){
+		err_quit("Usage: ./mayak [OPTIONS] <IP-Address>");
+	}
+
+	if ( socket_work(&sockfd) < 0 ){
+		err_sys("Wrong IP");
+	}
+	
 }
