0c368df9b8b0fda1ec2ffc67cd654bba11069fdd
hhri-yysh <heorhiiiukrainian2006@proton.me>
2025-03-17 22:38:21 +0200

    Updated main.c. Started using getopt_long from glibc (getopt.h).

--- src/main.c
+++ src/main.c
@@ -2,18 +2,6 @@
 #include "../lib/icmp.h"
 #include "../lib/socket.h"
 
-void example_print(){
-	printf("./mayak --help\n");
-	printf("Traceroute clone created by Heorhii Yashchuk\n");	
-}
-
-void print_help(){
-	printf("Usage: ./mayak [OPTIONS]\n");
-	printf("Options:\n");
-	printf("\t-4\t IPv4\n");
-	printf("\t-6\t IPv6\n");
-}
-
 void err_quit(const char* msg){
 	perror(msg);
 	exit(0);
@@ -25,21 +13,65 @@ void err_sys(const char* msg){
 }
 
 int main(int argc, char** argv){
-	int sockfd;
+	/*
+	 *	Example of working with glibc (getopt.h): 
+	 *	https://www.gnu.org/software/libc/manual/html_node/Getopt-Long-Option-Example.html
+	 * 
+	*/
 
-	if (argc <=1) {
-		example_print();
-	}
-	else if (strcmp(argv[1], "--help") == 0){
-		print_help();	
+	int sockfd, opt;
+
+	if (socket_work(&sockfd) < 0) {
+		err_quit("Failed to create socket");
 	}
 
-	if (argc != 2){
-		err_quit("Usage: ./mayak [OPTIONS] <IP-Address>");
-	}
-
-	if ( socket_work(&sockfd) < 0 ){
-		err_sys("Wrong IP");
-	}
+	while(1) {
+		static struct option long_opt[] = {
+			{"help", no_argument,   0,	'h'},
+			{"ipv4", no_argument,	0,	'4'},
+			{"ipv6", no_argument,   0,	'6'},
+			{"fqdn", no_argument,   0,	'f'},
+			{0, 0, 0, 0}
+		};
 	
+		int opt_index = 0;
+
+		opt = getopt_long(argc, argv, "46f", 
+					long_opt, &opt_index);
+		if ( opt == -1)
+			break;
+		switch (opt)
+		{
+			case 0: 
+				if (long_opt[opt_index].flag != 0)
+					break;
+				printf ("%s", long_opt[opt_index].name);
+				if (optarg)
+					printf("%s", optarg);
+				printf("\n");
+				break;
+			case 'h':
+				printf("\t --ipv4, -4\n");
+				printf("\t --ipv6, -6\n");
+				printf("\t --fqdn, -f\n");
+				break;
+			case '4':
+				printf("IPv6 select\n");
+				break;
+			case '6':
+				printf("IPv4 select\n");
+				break;
+			case 'f':
+
+				break;
+			case '?':
+		         //	printf("\n");
+			 //	err_quit("Unrecognized flags.\n");
+				break;
+			default:
+				abort();
+		}
+	}
+	close(sockfd);
+	exit(0);
 }
